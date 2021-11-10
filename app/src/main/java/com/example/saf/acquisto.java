35c6a1958b34151219fb55712fad2edb747f7646
Giorghen <a.giorgio21@studenti.unisa.it>
2021-11-10 10:26:22 +0100

    Completamento del progetto implementato con il controllo dell'inserimento dei dati all'atto dell'acquisto

--- app/src/main/java/com/example/saf/acquisto.java
+++ app/src/main/java/com/example/saf/acquisto.java
@@ -6,13 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class acquisto extends AppCompatActivity {
 
     private Button acquista;
     private ImageView back;
     private String nome;
+    private EditText tit, numCarta, cVv, indirizzo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +26,21 @@ public class acquisto extends AppCompatActivity {
 
         acquista= findViewById(R.id.acquisto);
         back= findViewById(R.id.CloseDialogImageView);
+        tit= findViewById(R.id.nome_titolare0);
+        numCarta= findViewById(R.id.numero_carta0);
+        cVv= findViewById(R.id.cvv0);
+        indirizzo= findViewById(R.id.consegna0);
         nome="";
 
         acquista.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (getIntent().getStringExtra("nomeProd") != null){
+                String t= tit.getText().toString();
+                String num= numCarta.getText().toString();
+                String cv= cVv.getText().toString();
+                String in= indirizzo.getText().toString();
+
+                if (getIntent().getStringExtra("nomeProd") != null && !t.equals("") && !num.equals("") && !cv.equals("") && !in.equals("") ){
                     DataBase db= new DataBase(acquisto.this);
                     nome= getIntent().getExtras().getString("nomeProd");
                     db.aggiungiOrdine(nome, "des ", 0.3);
@@ -34,7 +48,8 @@ public class acquisto extends AppCompatActivity {
                     intent.putExtra("nomeP", nome);
                     startActivity(intent);
                 }
-
+                else
+                    Toast.makeText(acquisto.this, "Errore nell'inserimento", Toast.LENGTH_SHORT).show();
             }
         });
 
